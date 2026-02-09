fe13be1fe54dc1255c4f815b9e9f4a501202a111
gwenaelpetit <gwenaelpetit59@gmail.com>
2026-02-09 13:06:29 +0000

    refactor(names): ecriture en clean code et ajout d'un test

--- exercices/mon_premier_script_avec_fonction.py
+++ exercices/mon_premier_script_avec_fonction.py
@@ -1,15 +1,39 @@
-"""
-Count names with more than seven letters
-"""
-def names(prenoms):
-    more_than_seven = 0
-    for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
-        else:
-            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-    return more_than_seven
+MIN_NAME_LENGTH = 7
 
-prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + str(names(prenoms=prenoms)))
+
+def count_names_longer_than(prenoms, min_length):
+    """
+    Count names with more than min_length characters.
+    """
+    count = 0
+
+    for prenom in prenoms:
+        if len(prenom) > min_length:
+            count += 1
+            print(prenom + " est un prénom avec un nombre de lettres supérieur à " + str(min_length))
+        else:
+            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à " + str(min_length))
+
+    return count
+
+
+if __name__ == "__main__":
+    prenoms = ["Guillaume","Gilles","Juliette","Antoine","François","Cassandre"]
+
+    result = count_names_longer_than(prenoms, MIN_NAME_LENGTH)
+
+    print(
+        "Nombre de prénoms dont le nombre de lettres est supérieur à "
+        + str(MIN_NAME_LENGTH)
+        + " : "
+        + str(result)
+    )
+
+
+def test_count_names_longer_than():
+    prenoms = ["Guillaume", "Gilles", "Juliette"]
+    assert count_names_longer_than(prenoms, 7) == 2
+    print("Test OK")
+
+
+test_count_names_longer_than()
